38c02a9d1da2d3d8f9b4aa35274827cab4e878d2
Dávid Török <torok.david@diak.remenyikzs.hu>
2025-03-18 18:02:33 +0100

    Calendar got a design update

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -1,20 +1,37 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
+import "./style/Calendar.css";
 
 function Calendar() {
-  const [selected, setSelected] = useState<Date>();
+    const [selected, setSelected] = useState<Date>();
 
-  return (
-    <DayPicker
-      mode="single"
-      selected={selected}
-      onSelect={setSelected}
-      footer={
-        selected ? `Selected: ${selected.toLocaleDateString()}` : "Pick a day."
-      }
-    />
-  );
+    return (
+        <div className="calendarContainer">
+            <h1>Set an alart</h1>
+            <br />
+            <form action="">
+                <DayPicker mode="single" selected={selected} onSelect={setSelected}
+                    footer={selected ? `Selected: ${selected.toLocaleDateString()}` : "Pick a day."} />
+                <br />
+                <label htmlFor="">
+                    Phone number:
+                    <input type="number" placeholder="Phone number" />
+                </label>
+                <br />
+                <label htmlFor="">
+                    Email:
+                    <input type="email" placeholder="Email" />
+                </label>
+                <br />
+                <label htmlFor="">
+                    Discription:
+                    <input type="text" placeholder="Discription" />
+                </label>
+            </form>
+            <br />
+        </div>
+    )
 }
 
 export default Calendar;
