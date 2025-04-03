fd452cbf2e511a3efec2242a4c7a9076996056ad
Dávid Török <torok.david@diak.remenyikzs.hu>
2025-04-03 11:34:24 +0200

    Navigation added to footer

--- src/Components/Footer/FooterSection.tsx
+++ src/Components/Footer/FooterSection.tsx
@@ -51,13 +51,12 @@ function FooterSection() {
                                     <h3>Useful Links</h3>
                                 </div>
                                 <ul>
-                                    <li><a href="#">Home</a></li>
+                                    <li><a href="/home">Home</a></li>
                                     <li><a href="#">Portfolio</a></li>
-                                    <li><a href="#">Contact</a></li>
+                                    <li><a href="/contact">Contact</a></li>
                                     <li><a href="#">About us</a></li>
-                                    <li><a href="#">Features</a></li>
-                                    <li><a href="#">Pricing</a></li>
-                                    <li><a href="#">Contact</a></li>
+                                    <li><a href="/features">Features</a></li>
+                                    <li><a href="/pricing">Pricing</a></li>
                                 </ul>
                             </div>
                         </div>
