450ede7dcc309c7e6d22ef6cfee818ef5c9992ba
Dávid Török <torok.david@diak.remenyikzs.hu>
2025-04-13 14:34:02 +0200

    the email notification is done

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -9,50 +9,56 @@ function Calendar() {
     const [selected, setSelected] = useState<Date>();
 
     //Alert spec datas
-    const emailAddresRef = useRef<HTMLInputElement>(null);
-    const descriptionRef = useRef<HTMLInputElement>(null);
+    const emailAddresRef = useRef<HTMLInputElement>(null); //email input
+    const descriptionRef = useRef<HTMLInputElement>(null); //description 
 
     const setAlert = (event: React.FormEvent) => {
         event.preventDefault();
 
-        const currentdate = new Date();
+        const email = emailAddresRef.current?.value.trim() ?? ""; //process email if undefiend or null its set to an an empty string
 
-        const email = emailAddresRef.current?.value.trim() ?? "";
-        const selectedIsValidDate = selected instanceof Date && !isNaN(selected.getTime());
+        //#region process the selected date 
+        const stringConvertedSelected = selected?.toLocaleString() || ""; //convert ot string if nul or undefined set to an empty sting
+        const splitedstringConvertedSelected = stringConvertedSelected.split(","); //split along , 
+        const selectedNumbers = splitedstringConvertedSelected[0].split("/"); //split along /
+
+        const splitedMount = +selectedNumbers[0]; //the list first value convert ot number 
+        const splitedDay = +selectedNumbers[1]; //the list seond value convert ot number 
+        const splitedYear = +selectedNumbers[2]; //the list threed value convert ot number 
+        //#endregion
+
+        //#region chack and compare the currend date 
+        const currentdate = new Date(); //the current date instance to get acces to the date parts
 
         const year = currentdate.getFullYear();
         const month = currentdate.getMonth() + 1;
-        const day = currentdate.getDate(); 
+        const day = currentdate.getDate();
 
-        const stringConvertedSelected = selected?.toLocaleString() || '';
-        const splitedstringConvertedSelected = stringConvertedSelected.split(',');
-        console.log(splitedstringConvertedSelected[0]);
+        // in the lower parts the program step by step compare the given date to the current date 
 
-        const selectedNumbers = splitedstringConvertedSelected[0].split('/');
+        const yearLessThen = splitedYear > year; //if the selcted year bigger then the actual year but not equal
+        const yearEqualThen = splitedYear === year; //if the selected year equal with the curent year it is necesarry to make continious compares with the mounth and days
 
-        const splitedMount = +selectedNumbers[0];
-        const splitedDay = +selectedNumbers[1];
-        const splitedYear = +selectedNumbers[2];
+        let mounthLessThen = false;
+        let mounthEqualThen = false;
+        let dayLessThen = false;
+        let dayEquaThen = false;
 
-        console.log(splitedMount + " splitedmounth");
-        console.log(splitedDay + " splitedday");
-        console.log(splitedYear + " splitedYear");
+        if (yearEqualThen) {
+            mounthLessThen = splitedMount < month; //if the selected month less then the ectual is not proper in this case 
+            mounthEqualThen = splitedMount === month //if the selcted month equal with the current it is necesary too to make compares with the days in the month 
+        }
+        if (mounthEqualThen) {
+            dayLessThen = splitedDay < day; //slected less then 
+            dayEquaThen = splitedDay === day; //selcted equal with
+        }
 
-        console.log(year + " current year");
-        console.log(month + " current month");
-        console.log(day + " current day");
+        const ullDateChack2 = (yearEqualThen || yearLessThen) && (mounthEqualThen || !mounthLessThen) && (!dayEquaThen && !dayLessThen);
+        //#endregion
 
-
-        console.log(splitedDay >= day, `${splitedDay} ${day}`);
-        console.log(splitedMount >= month, `${splitedMount} ${month}`);
-        console.log(splitedYear >= year, `${splitedYear} ${year}`);
-
-        const fullDateChack = splitedDay >= day && splitedMount >= month && splitedYear >= year;
-        console.log(fullDateChack);
-
-        if ((email !== "" && fullDateChack)) {
-            alert("Ok");
-            console.log(emailAddresRef.current?.value);
+        if ((email !== "" && ullDateChack2))  // if the entered email is valid and the given date is proper too
+        {
+            alert("Notofication email sent.");
             sendEmail("Notification set for " + selected, "We will notify you about the weather. " + descriptionRef.current?.value, email, email);
         } else {
             alert("Please enter a valid email address or a valid date.");
@@ -64,14 +70,17 @@ function Calendar() {
             <h1>Set an alert</h1>
             <br />
             <form action="submitButtom have been clickd">
+
+                {/* the actual calendar UI */}
                 <div className="calendar">
                     <DayPicker mode="single" selected={selected} onSelect={setSelected} className="calendar"
                         footer={selected ? `Selected: ${selected.toLocaleDateString()}` : "Pick a day."} />
                 </div>
 
                 <br />
-                <div>
 
+                {/* User data reguest UI */}
+                <div>
                     <label>
                         <span className="alartText">Email: </span>
                         <input className="inputStyle" type="email" placeholder="Email" ref={emailAddresRef} />
