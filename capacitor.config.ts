5d7be2b3756f62c71bf22da011d22081f34a7abd
6gDav <torokd1029@gmail.com>
2025-04-23 09:28:38 +0200

    Capacitor name fixed, footer feature nav link fixed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'weatherApp_no2',
+  appName: 'GetYWeather',
   webDir: 'dist'
 };
 
--- src/Components/Footer/FooterSection.tsx
+++ src/Components/Footer/FooterSection.tsx
@@ -66,7 +66,7 @@ function FooterSection() {
                                 <ul>
                                     <li><a href="/home">Home</a></li>
                                     <li><a href="/contact">Contact</a></li>
-                                    <li><a href="/features">Features</a></li>
+                                    <li><a href="/feature">Features</a></li>
                                     <li><a href="/pricing">Pricing</a></li>
                                 </ul>
                             </div>
